e1d5b67dac64907770cfdcd586c173d138dc1861
zihang <tlswlgkd565@gmail.com>
2024-08-19 14:41:44 +0900

    push

---
+++ src/main/java/org/example/githubactionpractice/GithubactionpracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.githubactionpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactionpracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubactionpracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/githubactionpractice/GithubactionpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubactionpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubactionpracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
